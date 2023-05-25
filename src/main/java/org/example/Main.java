7881d0f4335a84852d6221b535df974c1057074a
Kasper <kasper.berg1@hotmail.com>
2023-05-25 19:55:08 +0200

    gs

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,28 +1,11 @@
 package org.example;
 
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
+import com.google.gson.Gson;
 
 public class Main {
     public static void main(String[] args) {
-        String jsonString = "{\"name\":\"John\", \"age\":30, \"city\":\"New York\"}";
+        System.out.println("Hello world!");
 
-        JSONParser parser = new JSONParser();
-
-        try {
-            Object obj = parser.parse(jsonString);
-
-            JSONObject jsonObject = (JSONObject) obj;
-
-            String name = (String) jsonObject.get("name");
-            long age = (long) jsonObject.get("age");
-            String city = (String) jsonObject.get("city");
-
-            System.out.println("Name: " + name);
-            System.out.println("Age: " + age);
-            System.out.println("City: " + city);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        Gson gson = new Gson();
     }
 }
